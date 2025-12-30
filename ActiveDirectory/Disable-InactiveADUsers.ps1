2d333dfce59903a4d4b6769c611f7542e3028b04
ndu88 <62014243+ndu88@users.noreply.github.com>
2025-12-30 21:53:32 +0200

    Update Disable-InactiveADUsers.ps1

--- ActiveDirectory/Disable-InactiveADUsers.ps1
+++ ActiveDirectory/Disable-InactiveADUsers.ps1
@@ -1,13 +1,58 @@
 Import-Module ActiveDirectory
 
-$DaysInactive = 90
-$CutoffDate = (Get-Date).AddDays(-$DaysInactive)
+# Configuration
+$InactiveDays  = 90
+$WarningPeriod = 30
+$CutoffDate    = (Get-Date).AddDays(-$InactiveDays)
 
+$FromEmail   = "it-automation@company.co.za"
+$SMTPServer = "smtp.company.co.za"
+$LogFile    = "C:\Logs\InactiveUserWarning.log"
+
+# Get inactive users
 $InactiveUsers = Get-ADUser -Filter {
     Enabled -eq $true -and LastLogonDate -lt $CutoffDate
-} -Properties LastLogonDate
+} -Properties LastLogonDate, Manager, EmailAddress
 
 foreach ($User in $InactiveUsers) {
-    Disable-ADAccount -Identity $User.SamAccountName
-    Write-Output "Disabled user: $($User.SamAccountName)"
+
+    Write-Output "Processing user: $($User.SamAccountName)"
+
+    if ($User.Manager) {
+
+        $Manager = Get-ADUser $User.Manager -Properties EmailAddress, Name
+
+        if ($Manager.EmailAddress) {
+
+            $Subject = "Inactive AD Account Warning – $($User.Name)"
+            $Body = @"
+Good day $($Manager.Name),
+
+This is an automated notification to inform you that the following user account has been inactive for over $InactiveDays days:
+
+User Name: $($User.Name)
+Username: $($User.SamAccountName)
+Last Logon Date: $($User.LastLogonDate)
+
+If this employee is still part of the company, please advise them to log in within the next $WarningPeriod days.
+
+Failure to do so will result in the account being disabled in line with security compliance policies.
+
+If this account should remain active, please contact the IT team.
+
+Kind regards,
+IT Automation Team
+"@
+
+            Send-MailMessage `
+                -To $Manager.EmailAddress `
+                -From $FromEmail `
+                -Subject $Subject `
+                -Body $Body `
+                -SmtpServer $SMTPServer
+
+            # Log action
+            Add-Content -Path $LogFile -Value "$(Get-Date) | Warning sent to manager of $($User.SamAccountName)"
+        }
+    }
 }
